ffd8797629cd0a16ec15767f09592c154f95bac1
AguirreEze <aguirre.eze95@gmail.com>
2022-03-18 11:01:14 -0300

    Feat backend

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,20 +1,25 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 
-const initialData = [
+import {ProductType} from "../../../product/types";
+
+const initialData: ProductType[] = [
   {
     name: "Black t-shirt",
     price: 7.95,
+    image: "/products/shirt.png",
   },
   {
     name: "Black hoodie",
     price: 13,
+    image: "/products/hoodie.png",
   },
   {
     name: "Black cap",
     price: 23,
+    image: "/products/cap.png",
   },
 ];
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<ProductType[]>) {
   res.status(200).json(initialData);
 }
