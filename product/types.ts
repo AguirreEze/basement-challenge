c171fc4b2064560d876b095d5e9bd4e329f7f7f9
AguirreEze <aguirre.eze95@gmail.com>
2022-03-18 12:03:54 -0300

    feat checkout elem type

--- product/types.ts
+++ product/types.ts
@@ -1,5 +1,11 @@
 export interface ProductType {
   name: string;
+  description?: string;
   price: number;
   image: string;
+}
+
+export interface CheckoutElementType {
+  product: ProductType;
+  cant: number;
 }
