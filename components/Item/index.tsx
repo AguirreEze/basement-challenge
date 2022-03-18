da4f971cacda650a43eca6970193404b9e68661b
AguirreEze <aguirre.eze95@gmail.com>
2022-03-18 11:01:38 -0300

    Feat Item component on backend

--- components/Item/index.tsx
+++ components/Item/index.tsx
@@ -1,19 +1,15 @@
 import Image from "next/image";
 
-interface Iprops {
-  product: StaticImageData;
-  description: string;
-  price: number;
-}
+import {ProductType} from "../../product/types";
 
-export default function Item({product, description, price}: Iprops) {
+export default function Item({image, name, price}: ProductType) {
   return (
     <article className="cursor-pointer">
       <section className="border-b-2 bg-gradient-to-b from-black to-dark">
-        <Image alt={description} src={product} />
+        <Image alt={name} height={1156} src={image} width={880} />
       </section>
       <section className="flex justify-between py-3 text-lg">
-        <span className="font-semibold">{description}</span>
+        <span className="font-semibold">{name}</span>
         <span className="font-semibold">${price}</span>
       </section>
     </article>
